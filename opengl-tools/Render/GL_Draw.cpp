4f16baea795077c340da61657a1ffefcd37e332a
collebrusco <collebrusco@gmail.com>
2022-12-17 00:00:27 -0600

    Added GL_Draw rendering func

---
+++ opengl-tools/Render/GL_Draw.cpp
@@ -0,0 +1,18 @@
+//
+//  GL_Draw.cpp
+//  opengl-tools
+//
+//  Created by Frank Collebrusco on 12/16/22.
+//
+//
+#define GLFW_INCLUDE_GLCOREARB
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+#include "GL_Draw.h"
+
+void DrawMesh(MeshDetails& mesh){
+    glBindVertexArray(mesh.vao);
+    //TODO: error check this, glGetError()
+    glDrawElements((mesh.drawType == TRIANGLES) ? GL_TRIANGLES : GL_LINES, mesh.numElements, GL_UNSIGNED_INT, nullptr);
+    glBindVertexArray(0);
+}
---
+++ opengl-tools/Render/GL_Draw.h
@@ -0,0 +1,15 @@
+//
+//  GL_Draw.h
+//  opengl-tools
+//
+//  Created by Frank Collebrusco on 12/16/22.
+//
+//
+
+#ifndef GL_Draw_h
+#define GL_Draw_h
+#include "Vertex.h"
+
+void DrawMesh(MeshDetails* mesh);
+
+#endif /* GL_Draw_h */
