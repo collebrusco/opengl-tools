c694b6048862bed21f9688577374f524fd1a66d0
collebrusco <collebrusco@gmail.com>
2022-12-17 22:49:19 -0600

    add context file

---
+++ opengl-tools/Engine/Context.cpp
@@ -0,0 +1,9 @@
+//
+//  Context.cpp
+//  opengl-tools
+//
+//  Created by Frank Collebrusco on 12/17/22.
+//
+//
+
+#include "Context.h"
---
+++ opengl-tools/Engine/Context.h
@@ -0,0 +1,17 @@
+//
+//  Context.h
+//  opengl-tools
+//
+//  Created by Frank Collebrusco on 12/17/22.
+//
+//
+
+#ifndef Context_h
+#define Context_h
+
+class Context {
+    int what;
+    int the;
+};
+
+#endif /* Context_h */
